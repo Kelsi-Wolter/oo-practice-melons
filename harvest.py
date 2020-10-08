d94859370d2f3f5447fe893f07bfec3551016e71
vagrant <vagrant@vagrant.vm>
2020-10-08 22:01:02 +0000

    End of lab - need to complete part 2 last function (return a list of melon object)
    Co-authored-by: LadanShaie <ladan.shaie@gmail.com>

--- harvest.py
+++ harvest.py
@@ -80,7 +80,7 @@ def make_melon_type_lookup(melon_types):
     dict_melon = {}
     for fruit in melon_types:
         dict_melon[fruit.code] = fruit
-    print (dict_melon)
+    return dict_melon
 
 
 
@@ -90,19 +90,53 @@ def make_melon_type_lookup(melon_types):
 
 class Melon(object):
     """A melon in a melon harvest."""
+    def __init__(self, type, shape, color, field, harvester):
+        self.type = type
+        self.shape = shape
+        self.color = color
+        self.field = field
+        self.harvester = harvester
 
+    def is_sellable(self, shape, color, field):
+        if self.shape >= 5 and self.color >= 5 and self.field != 3:
+            return True
+            
     # Fill in the rest
     # Needs __init__ and is_sellable methods
 
 def make_melons(melon_types):
     """Returns a list of Melon objects."""
-
     # Fill in the rest
+
+    melons_by_id = make_melon_type_lookup(melon_types)
+
+
+    melon_1 = Melon(melons_by_id["yw"], 8, 7, 2, "Sheila")
+    melon_1.is_sellable()
+    melon_2 = Melon(melons_by_id["yw"], 3, 4, 2,"Sheila")
+    melon_3 = Melon(melons_by_id["yw"], 9, 8, 3, "Sheila")
+    melon_4 = Melon(melons_by_id["cas"], 10, 6, 35, "Sheila")
+    melon_5 = Melon(melons_by_id["cren"], 8, 9, 35, "Michael")
+    melon_6 = Melon(melons_by_id["cren"], 8, 2, 35, "Michael")
+    melon_7 = Melon(melons_by_id["cren"], 2, 3, 4, "Michael")
+    melon_8 = Melon(melons_by_id["musk"], 6, 7, 4, "Michael")
+    melon_9 = Melon(melons_by_id["yw"], 7, 10 , 3,"Sheila")
+
+    melon_list = [melon_1, melon_2, melon_3, melon_4, melon_5, melon_6, melon_7, melon_8, melon_9]
 
 def get_sellability_report(melons):
     """Given a list of melon object, prints whether each one is sellable."""
 
-    # Fill in the rest 
 
+    for fruit in melon_list:
+        
+
+        for pair in fruit.pairings:
+            pairing = pair     
+    
+        
+            print(f'Harvested by {Sheila} from Field {2} {(CAN BE SOLD)})
+            
+    # Fill in the rest 
 
 
